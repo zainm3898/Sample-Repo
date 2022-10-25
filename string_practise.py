95b6e53b18c9575830550874416eef809eeb7cea
Muhammad Zain <zainm3898@gmail.com>
2022-10-25 14:03:55 +0500

    changes

---
+++ string_practise.py
@@ -0,0 +1,16 @@
+message = "hello world"
+# print(len(message))
+# print(message[0])
+# print(message[0:5])
+# print(message[11])
+c = message.upper()
+# print(c)
+# print(message.count("ll"))
+# print(message.find("l"))
+# message = message.replace("world", "zain")
+# print(message)
+string_1 = "you are "
+string_2 = "my world"
+# print(string_1 + " " + string_2 + " " + "welcome to team")
+# print(f"{string_1}{string_2} Welcome ")
+# print(dir(string_1))
